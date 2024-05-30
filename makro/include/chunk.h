db72feccde46afe17fe955768d78466d007d8c0b
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-05-29 21:11:35 -0700

    Add support for basic chunks

---
+++ makro/include/chunk.h
@@ -0,0 +1,25 @@
+#ifndef makro_chunk
+#define makro_chunk
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OP_CONSTANT,
+  OP_RETURN,
+} OPCode;
+
+typedef struct {
+  int count;
+  int capacity;
+  uint8_t* code;
+  int* lines;
+  ValueArray constants;
+} Chunk;
+
+void initChunk(Chunk* chunk);
+void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
+
+#endif
