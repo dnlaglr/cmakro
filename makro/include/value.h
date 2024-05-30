3852b770e706a7c8b380d3f56fa401696b4160bf
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-05-29 21:10:56 -0700

    Add support for value arrays

---
+++ makro/include/value.h
@@ -0,0 +1,19 @@
+#ifndef makro_value
+#define makro_value
+
+#include "common.h"
+
+typedef double Value;
+
+typedef struct {
+  int count;
+  int capacity;
+  Value* values;
+} ValueArray;
+
+void initValueArray(ValueArray* array);
+void writeValueArray(ValueArray* array, Value value);
+void freeValueArray(ValueArray* array);
+void printValue(Value value);
+
+#endif
