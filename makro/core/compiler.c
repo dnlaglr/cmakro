ead02ec1b51159e48ef49b56f960c2e789578eab
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-05-30 01:32:59 -0700

    Add support for some literals

--- makro/core/compiler.c
+++ makro/core/compiler.c
@@ -144,6 +144,15 @@ static void binary() {
   }
 }
 
+static void literal() {
+  switch (parser.previous.type) {
+    case TOKEN_TRUE: emitByte(OP_TRUE); break;
+    case TOKEN_FALSE: emitByte(OP_FALSE); break;
+    case TOKEN_NULL: emitByte(OP_NULL); break;
+    default: return;
+  }
+}
+
 static void grouping() {
   expression();
   consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression");
@@ -151,7 +160,7 @@ static void grouping() {
 
 static void number() {
   double value = strtod(parser.previous.start, NULL);
-  emitConstant(value);
+  emitConstant(NUMBER_VAL(value));
 }
 
 static void unary() {
@@ -191,17 +200,17 @@ ParseRule rules[] = {
   [TOKEN_AND] = {NULL, NULL, PREC_NONE},
   [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
   [TOKEN_ELSE] = {NULL, NULL, PREC_NONE},
-  [TOKEN_FALSE] = {NULL, NULL, PREC_NONE},
+  [TOKEN_FALSE] = {literal, NULL, PREC_NONE},
   [TOKEN_FOR] = {NULL, NULL, PREC_NONE},
   [TOKEN_FUN] = {NULL, NULL, PREC_NONE},
   [TOKEN_IF] = {NULL, NULL, PREC_NONE},
-  [TOKEN_NULL] = {NULL, NULL, PREC_NONE},
+  [TOKEN_NULL] = {literal, NULL, PREC_NONE},
   [TOKEN_OR] = {NULL, NULL, PREC_NONE},
   [TOKEN_PRINT] = {NULL, NULL, PREC_NONE},
   [TOKEN_RETURN] = {NULL, NULL, PREC_NONE},
   [TOKEN_SUPER] = {NULL, NULL, PREC_NONE},
   [TOKEN_THIS] = {NULL, NULL, PREC_NONE},
-  [TOKEN_TRUE] = {NULL, NULL, PREC_NONE},
+  [TOKEN_TRUE] = {literal, NULL, PREC_NONE},
   [TOKEN_VAR] = {NULL, NULL, PREC_NONE},
   [TOKEN_WHILE] = {NULL, NULL, PREC_NONE},
   [TOKEN_ERROR] = {NULL, NULL, PREC_NONE},
--- makro/core/vm.c
+++ makro/core/vm.c
@@ -1,3 +1,4 @@
+#include <stdarg.h>
 #include <stdio.h>
 
 #include "../include/compiler.h"
@@ -9,6 +10,19 @@ VM vm;
 
 static void resetStack() {
   vm.stackTop = vm.stack;
+}
+
+static void runtimeError(const char* format, ...) {
+  va_list args;
+  va_start(args, format);
+  vfprintf(stderr, format, args);
+  va_end(args);
+  fputs("\n", stderr);
+
+  size_t instruction = vm.ip - vm.chunk->code - 1;
+  int line = vm.chunk->lines[instruction];
+  fprintf(stderr, "[line %d] in script\n", line);
+  resetStack();
 }
 
 void initVM() {
@@ -29,10 +43,23 @@ Value pop() {
   return *vm.stackTop;
 }
 
+static Value peek(int distance) {
+  return vm.stackTop[-1 - distance];
+}
+
 static InterpretResult run() {
   #define READ_BYTE() (*vm.ip++)
   #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
-  #define BINARY_OP(op) do { double b = pop(); double a = pop(); push(a op b); } while (false)
+  #define BINARY_OP(valueType, op) \
+    do { \
+      if (!IS_NUMBER(peek(0)) || !IS_NUMBER(peek(1))) { \
+        runtimeError("Operands must be numbers."); \
+        return INTERPRET_RUNTIME_ERROR; \
+      } \
+      double b = AS_NUMBER(pop()); \
+      double a = AS_NUMBER(pop()); \
+      push(valueType(a op b)); \
+    } while (false)
 
   for (;;) {
     #ifdef DEBUG_TRACE_EXECUTION
@@ -55,20 +82,33 @@ static InterpretResult run() {
         Value constant = READ_CONSTANT();
         push(constant);
         break;
+      case OP_TRUE:
+        push(BOOL_VAL(true));
+        break;
+      case OP_FALSE:
+        push(BOOL_VAL(false));
+        break;
+      case OP_NULL:
+        push(NULL_VAL);
+        break;
       case OP_ADD:
-        BINARY_OP(+);
+        BINARY_OP(NUMBER_VAL, +);
         break;
       case OP_SUBTRACT:
-        BINARY_OP(-);
+        BINARY_OP(NUMBER_VAL, -);
         break;
       case OP_MULTIPLY:
-        BINARY_OP(*);
+        BINARY_OP(NUMBER_VAL, *);
         break;
       case OP_DIVIDE:
-        BINARY_OP(/);
+        BINARY_OP(NUMBER_VAL, /);
         break;
       case OP_NEGATE:
-        push(-pop());
+        if (!IS_NUMBER(peek(0))) {
+          runtimeError("Operand must be a number");
+          return INTERPRET_RUNTIME_ERROR;
+        }
+        push(NUMBER_VAL(-AS_NUMBER(pop())));
         break;
       case OP_RETURN:
         printValue(pop());
